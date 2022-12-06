d5c44e3fe81de349892e712c4015313cc0d93ef4
ranveeryadav0311 <119935655+ranveeryadav0311@users.noreply.github.com>
2022-12-06 16:21:05 +0530

    Update LinearSearch.java

--- Searching/LinearSearch.java
+++ Searching/LinearSearch.java
@@ -1,1 +1,9 @@
 //Linear Search
+  public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
